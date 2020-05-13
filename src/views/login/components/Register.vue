58a86a0763b4598e76b5ca86d645a3922b3e3a07
Nannan <1041836312@qq.com>
2020-05-14 00:08:22 +0800

    注册界面

---
+++ src/views/login/components/Register.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="register-container">
+    <el-form ref="ruleForm" :model="ruleForm" status-icon :rules="rules" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="ruleForm.username" maxlength="16" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input v-model="ruleForm.pass" type="password" maxlength="16" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input v-model="ruleForm.checkPass" type="password" maxlength="16" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="昵称" prop="nickname">
+        <el-input v-model="ruleForm.nickname" maxlength="16" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="ruleForm.email" maxlength="64" autocomplete="off" />
+      </el-form-item>
+      <div class="reg-btn">
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </div>
+
+    </el-form>
+  </div>
+</template>
+
+<script>
+// import openWindow from '@/utils/open-window'
+
+export default {
+  name: 'Register',
+  data() {
+    var checkUsername = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('请输入用户名'))
+      }
+      var reg = /^[a-zA-Z]\w{4,15}$/
+      if (!reg.test(value)) {
+        return callback(new Error('字母开头，5-16字节，字母数字下划线组合'))
+      }
+      callback()
+    }
+    var checkEmail = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('请输入邮箱'))
+      }
+      var reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+      if (!reg.test(value)) {
+        return callback(new Error('邮箱格式错误'))
+      }
+      callback()
+    }
+    var checkNickname = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('请输入昵称'))
+      }
+      var reg = /^[\u4e00-\u9fa5_a-zA-Z0-9]+$/
+      if (!reg.test(value)) {
+        return callback(new Error('中文、英文、数字及下划线'))
+      }
+      callback()
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        var reg = /^[\u0021-\u007E]{6,16}$/
+        if (!reg.test(value)) {
+          return callback(new Error('长度6-16'))
+        }
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      ruleForm: {
+        username: '',
+        pass: '',
+        checkPass: '',
+        nickname: '',
+        email: ''
+      },
+      rules: {
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { validator: validatePass2, trigger: ['change', 'blur'] }
+        ],
+        username: [
+          { validator: checkUsername, trigger: 'blur' }
+        ],
+        nickname: [
+          { validator: checkNickname, trigger: 'blur' }
+        ],
+        email: [
+          { validator: checkEmail, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .reg-btn{
+    text-align: center;
+  }
+.demo-ruleForm input{
+  color: #606266;
+}
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">登录</h3>
       </div>
 
       <el-form-item prop="username">
@@ -13,7 +13,7 @@
         <el-input
           ref="username"
           v-model="loginForm.username"
-          placeholder="Username"
+          placeholder="用户名"
           name="username"
           type="text"
           tabindex="1"
@@ -31,7 +31,7 @@
             ref="password"
             v-model="loginForm.password"
             :type="passwordType"
-            placeholder="Password"
+            placeholder="密码"
             name="password"
             tabindex="2"
             autocomplete="on"
@@ -45,60 +45,46 @@
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:80%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <div style="position:relative">
-        <div class="tips">
-          <span>Username : admin</span>
-          <span>Password : any</span>
-        </div>
-        <div class="tips">
-          <span style="margin-right:18px;">Username : editor</span>
-          <span>Password : any</span>
-        </div>
+      <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
+        注册
+      </el-button>
 
-        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          Or connect with
-        </el-button>
-      </div>
     </el-form>
 
-    <el-dialog title="Or connect with" :visible.sync="showDialog">
-      Can not be simulated on local, so please combine you own business simulation! ! !
-      <br>
-      <br>
-      <br>
-      <social-sign />
+    <el-dialog width="500px" title="注册" :visible.sync="showDialog">
+      <Register />
     </el-dialog>
   </div>
 </template>
 
 <script>
 import { validUsername } from '@/utils/validate'
-import SocialSign from './components/SocialSignin'
+import Register from './components/Register'
 
 export default {
   name: 'Login',
-  components: { SocialSign },
+  components: { Register },
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入正确的用户名'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码不能少于6位'))
       } else {
         callback()
       }
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
@@ -135,7 +121,7 @@ export default {
     }
   },
   destroyed() {
-    // window.removeEventListener('storage', this.afterQRScan)
+    // window.removeEventListener('storaconnect ge', this.afterQRScan)
   },
   methods: {
     checkCapslock(e) {
@@ -256,6 +242,8 @@ $light_gray:#eee;
   min-height: 100%;
   width: 100%;
   background-color: $bg;
+  /*background: url("http://www.lanct.com/uploads/allimg/20200106/1-20010621425CK.png");*/
+  /*background-repeat: no-repeat;*/
   overflow: hidden;
 
   .login-form {
@@ -311,8 +299,7 @@ $light_gray:#eee;
 
   .thirdparty-button {
     position: absolute;
-    right: 0;
-    bottom: 6px;
+    width: 15%;
   }
 
   @media only screen and (max-width: 470px) {
