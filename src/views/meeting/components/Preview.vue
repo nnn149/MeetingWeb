09432b740ee9c92a7dd0a55ae7d32a71be70ba12
Nannan <1041836312@qq.com>
2020-05-15 23:51:01 +0800

    会议界面预览窗口设计

---
+++ src/views/meeting/components/Preview.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="Preview-container">
+    <div style="background: black">
+      <div class="video-container">
+        <video style="height: 100%;width: 100%" />
+      </div>
+    </div>
+
+    <div class="console-container">
+      <span style="max-width: 130px">
+        <i class="el-icon-s-custom" />
+        <span>你你你你你你</span>
+      </span>
+      <span>
+        <i class="el-icon-chat-dot-round" />
+        <svg-icon icon-class="eye-open" />
+        <i class="el-icon-turn-off-microphone" />
+        <i class="el-icon-full-screen" />
+        <i style="color: #ff4250;horiz-align: right" class="el-icon-error" />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Preview'
+}
+</script>
+
+<style lang="scss" scoped>
+.Preview-container{
+  display:inline-block;
+  width: 260px;
+  height: 200px;
+  margin-right: 3px;
+}
+  .video-container{
+    height: 170px;
+    background: #939394;
+    -webkit-mask-image: url("~@/assets/svg/people.svg");
+    mask-image: url("~@/assets/svg/people.svg");
+    mask-repeat: no-repeat;
+    mask-position: center;
+  }
+  .console-container{
+    height: 26px;
+    background: white;
+    line-height:30px;
+  }
+.console-container i {
+  font-size: 22px;
+}
+.console-container .svg-icon {
+  font-size: 22px;
+}
+
+</style>
--- src/views/meeting/index.vue
+++ src/views/meeting/index.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="meeting-container">
-    <video ref="video_self" autoplay playsinline />
-    <el-button @click="startV()">开始</el-button>
-    <el-button @click="stopV()">停止</el-button>
+
+    <el-container>
+      <el-header height="214px">
+        <preview />
+        <preview />
+        <preview />
+        <preview />
+        <preview />
+        <preview />
+        <preview />
+        <preview />
+        <preview />
+      </el-header>
+      <el-container>
+
+        <el-main>
+          <video ref="video_self" autoplay playsinline />
+          <el-button @click="startV()">开始</el-button>
+          <el-button @click="stopV()">停止</el-button>
+
+          <el-button type="primary" style="margin-left: 16px;" @click="addV()">
+            添加
+          </el-button>
+
+        </el-main>
+        <el-aside width="300px">Aside</el-aside>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Preview from './components/Preview'
 
 export default {
   name: 'Meeting',
+  components: { Preview },
   data() {
     return {
+
       localStream: undefined
     }
   },
@@ -35,6 +63,9 @@ export default {
       this.localStream.getTracks().forEach(function(track) {
         track.stop()
       })
+    },
+    addV() {
+
     }
   }
 
@@ -42,4 +73,27 @@ export default {
 </script>
 
 <style lang="scss">
+  .el-header{
+    background-color: #B3C0D1;
+    padding: 0;
+    margin: 0;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    margin: 0;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+
+    padding: 0;
+  }
+  .el-container{
+    height: calc(100vh - 84px)
+  }
+
 </style>
