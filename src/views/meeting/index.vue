96675f1ccf938048b5e7af2b707109baee5acd3e
Nannan <1041836312@qq.com>
2020-05-17 12:14:46 +0800

    创建进入房间窗口

--- src/views/meeting/index.vue
+++ src/views/meeting/index.vue
@@ -6,13 +6,12 @@
         <preview v-for="client in clients" :key="client.userId" :client="client" @pmEvent="pm" @microEvent="changeMicro" @fullEvent="fullScreen" @kickEvent="kick" @viewEvent="changeView" />
       </el-header>
       <el-container>
-
         <el-main>
-          <video ref="video_self" autoplay playsinline />
+          <video ref="video_full" autoplay playsinline />
           <el-button @click="startV()">开始</el-button>
           <el-button @click="stopV()">停止</el-button>
 
-          <el-button type="primary" style="margin-left: 16px;" @click="addV()">
+          <el-button type="primary" style="margin-left: 16px;" @click="addV">
             添加
           </el-button>
 
@@ -20,6 +19,22 @@
         <el-aside width="350px"><Chat :receive-msg="receiveMsg" @chatEvent="sendChat" @noticeEvent="notice" /></el-aside>
       </el-container>
     </el-container>
+
+    <el-dialog title="请输入房间号和密码：" :visible.sync="dialogFormVisible" @close="closeView">
+      <el-form ref="romeForm" :model="roomFromDate" status-icon :rules="roomFromRules" label-width="100px">
+        <el-form-item label="房间号:" prop="roomId">
+          <el-input v-model="roomFromDate.roomId" maxlength="10" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="密码:" prop="roomPw">
+          <el-input v-model="roomFromDate.roomPw" type="password" maxlength="10" autocomplete="off" />
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="createOrEnterRoom('enter')">加 入</el-button>
+          <el-button type="primary" @click="createOrEnterRoom('create')">创 建</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -33,33 +48,69 @@ export default {
   name: 'Meeting',
   components: { Preview, Chat },
   data() {
+    var valiRoomId = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入5-10位纯数字'))
+      } else {
+        var reg = /^\d{5,10}$/
+        if (!reg.test(value)) {
+          return callback(new Error('请输入5-10位纯数字'))
+        }
+        callback()
+      }
+    }
     return {
+      dialogFormVisible: false,
       localWebsocket: undefined,
-      wsurl: undefined,
+      wsUrl: undefined,
       receiveMsg: '',
+      isInRoom: false,
       clients: [{
         userId: '0',
         roomId: '0',
         localStream: undefined
-      }]
+      }],
+      roomFromDate: {
+        roomId: '',
+        roomPw: ''
+      },
+      roomFromRules: {
+        roomId: [
+          { validator: valiRoomId, trigger: 'blur' }
+        ],
+        roomPw: [
+          { validator: valiRoomId, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  async mounted() {
+    this.dialogFormVisible = true
+    try {
+      await this.initLocalWebsocket()
+    } catch (e) {
+      console.log('websocket错误:' + e.message())
+      this.$message.error('网络连接错误!')
+      this.closeView()
     }
   },
   beforeDestroy() {
     console.log('即将销毁')
   },
   destroyed() {
-    this.localWebsocket.close() // 离开路由之后断开localWebsocket连接
+    if (this.localWebsocket !== undefined) {
+      this.localWebsocket.close() // 离开路由之后断开localWebsocket连接
+    }
   },
   methods: {
     // 设置本地播放器
     startV() {
-      this.initLocalWebsocket()
       console.log(adapter.browserDetails.browser)
       navigator.mediaDevices.getUserMedia({ video: true })
         .then((mediaStream) => {
           console.log('本地播放器设置')
           const c0 = {
-            userId: '00',
+            userId: '0',
             roomId: '0',
             localStream: mediaStream
           }
@@ -97,6 +148,7 @@ export default {
     },
     fullScreen(userId) {
       console.log('fullScreen:' + userId)
+      this.$refs.video_full.srcObject = this.clients[userId].localStream
     },
     kick(userId) {
       console.log('kick:' + userId)
@@ -113,11 +165,31 @@ export default {
     receiveMsgHandle(msg) {
       this.receiveMsg = 'nnn:' + new Date()
     },
-    async  initLocalWebsocket() { // 初始化weosocket
+    createOrEnterRoom(method) {
+      this.$refs.romeForm.validate((valid) => {
+        if (valid) {
+          if (method === 'create') {
+            console.log('创建房间:' + this.roomFromDate.roomId)
+          } else {
+            console.log('加入房间:' + this.roomFromDate.roomId)
+          }
+        } else {
+          console.log('表单验证错误')
+          return false
+        }
+      })
+    },
+    closeView() {
+      if (this.isInRoom === false) {
+        this.$router.go(-1)
+      }
+    },
+    async  initLocalWebsocket() {
+      console.log('初始化weosocket')
       const response = await getUrl()
-      this.wsurl = response.data
-      console.log('获取到wsurl:' + this.wsurl)
-      this.localWebsocket = new WebSocket(this.wsurl)
+      this.wsUrl = response.data
+      console.log('获取到wsurl:' + this.wsUrl)
+      this.localWebsocket = new WebSocket(this.wsUrl)
       this.localWebsocket.onmessage = this.wseReceiveMessage
       this.localWebsocket.onopen = () => {
         console.log('localWebsocket打开')
@@ -137,6 +209,7 @@ export default {
     wsSend(Data) { // 数据发送
       this.localWebsocket.send(Data)
     }
+
   }
 
 }
